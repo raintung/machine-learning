9a9715dbdccb62bcaeb78a06e3bc66fc0606c58d
raintung <raintung.li@gmail.com>
2016-12-24 15:16:19 +0800

    wordcount

---
+++ scala/src/spark/split/wordcount.scala
@@ -0,0 +1,21 @@
+package spark.split
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+
+object wordcount {
+   def main(args: Array[String]) {
+      if (args.length < 1) {
+       System.err.println("Usage: <file>")
+       //System.exit(1)
+     }
+     val conf = new SparkConf()
+     val sc = new SparkContext(conf)
+     val line = sc.textFile(args(0))
+     println("start the count");
+     line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
+     sc.stop()
+   }
+} 
