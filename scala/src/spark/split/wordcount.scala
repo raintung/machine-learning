94dcbd09d6e8349a0caed69b37d5d2ea8ad48bea
raintung <raintung.li@gmail.com>
2017-02-08 11:24:42 +0800

    update the new file

--- scala/src/spark/split/wordcount.scala
+++ scala/src/spark/split/wordcount.scala
@@ -7,15 +7,16 @@ import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 
 object wordcount {
    def main(args: Array[String]) {
-      if (args.length < 1) {
-       System.err.println("Usage: <file>")
-       //System.exit(1)
-     }
-     val conf = new SparkConf()
+     val conf = new SparkConf().setAppName("wordcount").setMaster("spark://192.168.121.101:7077")
      val sc = new SparkContext(conf)
-     val line = sc.textFile(args(0))
-     println("start the count");
-     line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
+     sc.addJar("/Users/keli/Documents/machinelearning.jar")
+     //val sc = new SparkContext("spark://172.16.114.8:7077","wordcount",System.getenv("SPARK_HOME"),SparkContext.jarOfClass(this.getClass).toSeq, Map())
+     val line = sc.textFile("hdfs://192.168.121.101:9000/data/word.txt")
+     // val line = sc.textFile(args(0))
+     println("start the count1");
+     line.flatMap(_.split(" "))
+                         .map((_, 2))
+                             .reduceByKey(_+_).collect().foreach(println)
      sc.stop()
    }
 } 
